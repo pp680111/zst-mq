2b17119bebf0db9380a54705949b0117d6c3005e
zhangshutao <zhangshutao@ava.com.cn>
2024-07-09 20:33:25 +0800

    feat(client): client模块代码初始化

---
+++ zst-mq-client/src/main/java/com/zst/mq/client/BrokerProperties.java
@@ -0,0 +1,11 @@
+package com.zst.mq.client;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BrokerProperties {
+    private String host;
+    private int port;
+}
---
+++ zst-mq-client/src/main/java/com/zst/mq/client/NettyTransport.java
@@ -0,0 +1,58 @@
+package com.zst.mq.client;
+
+import com.zst.mq.broker.transport.FrameDecoder;
+import com.zst.mq.broker.transport.FrameEncoder;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class NettyTransport {
+    private EventLoopGroup eventLoopGroup;
+    private Channel channel;
+    private BrokerProperties brokerProperties;
+
+    public void start() {
+        eventLoopGroup = new NioEventLoopGroup(Runtime.getRuntime().availableProcessors());
+
+        try {
+            Bootstrap bootstrap = new Bootstrap()
+                    .group(eventLoopGroup)
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .channel(NioSocketChannel.class)
+                    .handler(new Initializer());
+
+            channel = bootstrap.connect(brokerProperties.getHost(),
+                    brokerProperties.getPort()).channel();
+
+            channel.closeFuture().addListener(future -> {
+                stop();
+            });
+
+            log.info("netty client connection start");
+        } catch (Exception e) {
+            log.info("netty client connect failed", e);
+        }
+    }
+
+    public void stop() {
+        if (eventLoopGroup != null && !eventLoopGroup.isShutdown()) {
+            eventLoopGroup.shutdownGracefully();
+            eventLoopGroup = null;
+        }
+    }
+
+    private static class Initializer extends ChannelInitializer {
+        @Override
+        protected void initChannel(Channel channel) throws Exception {
+            channel.pipeline()
+                    .addLast(new FrameDecoder())
+                    .addLast(new FrameEncoder());
+        }
+    }
+}
